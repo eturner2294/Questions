0712758b28dfb94a3ac02789f2817f00e07be35b
Elsie Turner <eturner2294@gmail.com>
2024-06-14 12:01:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Pleasse enter a string of letters for Q1");
+
+Questions questions = new Questions();
+String input = Console.ReadLine();
+Console.WriteLine(questions.getWrongAnswers(input.Length, input));
+
+Console.WriteLine("Please enter 3 characters for Q2");
+input = Console.ReadLine();
+
+Char[] stringToChar = input.ToCharArray();
+Console.WriteLine(questions.GetSum(stringToChar[0], stringToChar[1], stringToChar[3]));  
+
+
---
+++ Questions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Questions
+    {
+        public String getWrongAnswers(int N, String c)
+        {
+            String newString = " "; 
+            foreach (Char newChar in c) { 
+                if (newChar == 'a')
+                {
+                    newString += 'b'; 
+                } else if (newChar == 'b')
+                {
+                    newString += 'a'; 
+                }
+                else
+                {
+                    Console.Write("String should only contains a and b");
+                    break; 
+                }
+            }
+
+            return newString; 
+        }
+
+        public int GetSum(char a, char b, char c)
+        {
+            return valueOfLetter(a) + valueOfLetter(b) + valueOfLetter(c); 
+        }
+
+        private int valueOfLetter(char letter)
+        {
+            switch (letter)
+            {
+                case 'a':
+                    return 1;
+                case 'b':
+                    return 2;
+                case 'c':
+                    return 3; 
+            }
+
+            //should not get here but if we do, return 0
+            return 0; 
+        }
+    }
+}
